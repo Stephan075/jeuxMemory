b4fae9537019d258c99924de80b9a6416530c931
Stephan <Stephan-folio@hotmail.fr>
2019-04-15 12:11:25 +0200

    Edit GameManager.js

--- js/GameManager.js
+++ js/GameManager.js
@@ -1,23 +1,33 @@
 var GameManager = function(){
     var _this = this;
-    //
+    //créer le div #time-display
     this.timeDisplay = '<div id="time-display"></div>';
     this.container;
+    this.timer;
     // Méthdes
     this.initTime = function(){
+       // console.log(document.getElementsByClassName("container"));
+        
         //Ajout de l'élement dans l'html
         this.container = document.getElementsByClassName("container")[0];
         this.container.innerHTML = _this.timeDisplay;
         // mettre en place des chiffres 0:00
-        document.getElementById("time-display".innerHTML = "00:00")
+        document.getElementById("time-display").innerHTML = "00:00";
+        this.timer = setInterval(this.changeTimeDisplay, 1000);
     }
     //
     this.initDisplay = function (){
         //Redimension du container pour qu'il fasse la hauteur de l'écran
-       this.container.style.height
+       this.container.style.height = window.innerHeight + "px"
+    }
+    //Listener
+    this.changeTimeDisplay = function (){
+        console.log("une Seconde !!!");
+        
     }
 
     // Appel de la méthode.
     this.initTime();
+    this.initDisplay();
 
 };
